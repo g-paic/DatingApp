e03b19034e99a174a27d24fddb69bf0bd9352852
Gabix123 <gabriel.paic@fer.hr>
2026-07-18 15:34:21 +0200

    section-9

--- Client/src/core/services/account-service.ts
+++ Client/src/core/services/account-service.ts
@@ -2,16 +2,16 @@ import { HttpClient } from '@angular/common/http';
 import { inject, Injectable, signal } from '@angular/core';
 import { LoginCreds, RegisterCreds, User } from '../../types/user';
 import { tap } from 'rxjs';
+import { environment } from '../../environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AccountService {
   private http = inject(HttpClient);
+  private baseUrl = environment.apiUrl;
   currentUser = signal<User | null>(null);
   
-  baseUrl = "https://localhost:5001/api/";
-
   register(creds: RegisterCreds) {
     return this.http.post<User>(this.baseUrl + 'account/register', creds).pipe(
       tap(user => {
