44d428ed3f5960d345cf0762a7e6fb5eb8c575d7
Gabix123 <gabriel.paic@fer.hr>
2026-08-08 23:43:08 +0200

    section-10

--- API/obj/Debug/net10.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net10.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cef06e0e46380ee92e8f27ca5239cbc4914e2c87")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e03b19034e99a174a27d24fddb69bf0bd9352852")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
